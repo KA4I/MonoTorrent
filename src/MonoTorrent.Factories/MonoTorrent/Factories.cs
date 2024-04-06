5e2c061bf283262d4eedda0c531cdbab1975bb63
Victor Nova <lostfreeman@gmail.com>
2024-04-05 17:25:35 -0700

    pass remote peer ID to TemporaryLocalPeerIdGenerator

--- src/MonoTorrent.Factories/MonoTorrent/Factories.cs
+++ src/MonoTorrent.Factories/MonoTorrent/Factories.cs
@@ -58,7 +58,7 @@ namespace MonoTorrent
         public delegate IDhtEngine DhtCreator ();
         public delegate IDhtListener DhtListenerCreator (IPEndPoint endpoint);
         public delegate HttpClient HttpClientCreator (AddressFamily family);
-        public delegate ReusableTask<BEncodedString> TemporaryLocalPeerIdGenerator (BEncodedString permanentLocalPeerId, InfoHash infoHash, Uri peerUri);
+        public delegate ReusableTask<BEncodedString> TemporaryLocalPeerIdGenerator (BEncodedString permanentLocalPeerId, BEncodedString peerId, InfoHash infoHash, Uri peerUri);
         public delegate ILocalPeerDiscovery LocalPeerDiscoveryCreator ();
         public delegate IPeerConnection PeerConnectionCreator (Uri uri);
         public delegate IPeerConnectionListener PeerConnectionListenerCreator (IPEndPoint endPoint);
@@ -101,7 +101,7 @@ namespace MonoTorrent
 
             HttpClientFunc = HttpRequestFactory.CreateHttpClient;
 
-            TemporaryLocalPeerIdGeneratorFunc = (permanentLocalPeerId, _, __) => ReusableTask.FromResult (permanentLocalPeerId);
+            TemporaryLocalPeerIdGeneratorFunc = (permanentLocalPeerId, _, __, ___) => ReusableTask.FromResult (permanentLocalPeerId);
             LocalPeerDiscoveryFunc = () => new LocalPeerDiscovery ();
             PeerConnectionFuncs = new ReadOnlyDictionary<string, PeerConnectionCreator> (
                 new Dictionary<string, PeerConnectionCreator> {
@@ -176,8 +176,9 @@ namespace MonoTorrent
             return dupe;
         }
 
-        public ReusableTask<BEncodedString> CreateTemporaryLocalPeerIdAsync (BEncodedString permanentLocalPeerId, InfoHash infoHash, Uri peerUri)
-            => TemporaryLocalPeerIdGeneratorFunc (permanentLocalPeerId, infoHash, peerUri);
+        public ReusableTask<BEncodedString> CreateTemporaryLocalPeerIdAsync (BEncodedString permanentLocalPeerId,
+            BEncodedString peerId, InfoHash infoHash, Uri peerUri)
+            => TemporaryLocalPeerIdGeneratorFunc (permanentLocalPeerId, peerId, infoHash, peerUri);
         public Factories WithTemporaryLocalPeerIdGenerator (TemporaryLocalPeerIdGenerator generator)
         {
             var dupe = MemberwiseClone ();
