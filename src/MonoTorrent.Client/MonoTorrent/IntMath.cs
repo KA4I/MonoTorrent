7e39d28e1b8cda4f974874e6a79b4965bd7ade54
Victor Nova <lostfreeman@gmail.com>
2024-05-14 01:50:53 -0700

    IntMath was severely broken...

--- src/MonoTorrent.Client/MonoTorrent/IntMath.cs
+++ src/MonoTorrent.Client/MonoTorrent/IntMath.cs
@@ -12,7 +12,7 @@ namespace MonoTorrent
                 return 1;
             return (power & 1) == 0
                 ? PowUnchecked (value * value, power >> 1)
-                : value * PowUnchecked (value * value, power >> 1);
+                : value * PowUnchecked (value, power - 1);
         }
 
         public static long Pow (long value, int power)
@@ -21,9 +21,9 @@ namespace MonoTorrent
                 throw new ArgumentOutOfRangeException (nameof (power), "Power must be greater than or equal to zero");
             if (power == 0)
                 return 1;
-            return checked(power & 1) == 0
+            return checked((power & 1) == 0
                 ? Pow (value * value, power >> 1)
-                : value * Pow (value * value, power >> 1);
+                : value * Pow (value, power - 1));
         }
 
         public static int Pow (int value, int power)
@@ -32,9 +32,9 @@ namespace MonoTorrent
                 throw new ArgumentOutOfRangeException (nameof (power), "Power must be greater than or equal to zero");
             if (power == 0)
                 return 1;
-            return checked(power & 1) == 0
+            return checked((power & 1) == 0
                 ? Pow (value * value, power >> 1)
-                : value * Pow (value * value, power >> 1);
+                : value * Pow (value, power - 1));
         }
     }
 }
